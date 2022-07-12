51ccd3c9f2767b0eaa849f3e8465806221111d7c
Tien Dao <tiendao5240@gmail.com>
2022-07-11 23:44:22 -0400

    Added iOS Clean code

---
+++ ios/ReactDeviceInfoMethod.swift
@@ -0,0 +1,57 @@
+//
+//  ReactDeviceInfoMethod.swift
+//  MinhTienDao_Lab2_NativeModules
+//
+//  Created by Tien Dao on 2022-07-11.
+//
+
+import Foundation
+
+@objc(ReactDeviceInfoMethod)
+class ReactDeviceInfoMethod: NSObject {
+  
+  private var arrayResult: [String] = []
+  
+  @objc
+    func getPhoneID(_ resolve:RCTPromiseResolveBlock,
+                         reject:RCTPromiseRejectBlock){
+
+      let currentDevice = UIDevice.self
+
+      let deviceId = currentDevice.current.identifierForVendor!.uuidString
+
+      let deviceType = currentDevice.current.model
+
+      let deviceName = currentDevice.current.name
+
+      let deviceHardware = currentDevice.current.model
+
+      let sysVersion = currentDevice.current.systemVersion
+
+      let deviceLang = Locale.preferredLanguages[0]
+
+      let bundleId = Bundle.main.bundleIdentifier
+
+      let builderNumber = Bundle.main.infoDictionary!["CFBundleShortVersionString"]
+
+      
+
+      do {
+
+
+        arrayResult.append(deviceId)
+        arrayResult.append(deviceType)
+        arrayResult.append(deviceName)
+        arrayResult.append(deviceHardware)
+        arrayResult.append(sysVersion)
+        arrayResult.append(deviceLang)
+        arrayResult.append(bundleId!)
+        arrayResult.append(builderNumber as! String)
+        
+        resolve(arrayResult)
+      }catch{
+
+        reject("ERROR", "Cannot get device info", error)
+      }
+      }
+}
---
+++ ios/ReactFuelMethod.swift
@@ -0,0 +1,45 @@
+//
+//  ReactFuelMethod.swift
+//  MinhTienDao_Lab2_NativeModules
+//
+//  Created by Tien Dao on 2022-07-11.
+//
+
+import Foundation
+
+@objc(ReactFuelMethod)
+class ReactFuelMethod: NSObject {
+  private var arrayResult: [String] = []
+  
+  @objc
+  func getFuelData(_ resolve:RCTPromiseResolveBlock,
+                   reject:RCTPromiseRejectBlock) {
+    do {
+      let userMaxAllowance = "600";
+      let fuelType1 = "Petrol";
+      let fuelType2 = "Diesel";
+      let fuelType3 = "BatteryCharge";
+
+      let pricePerLiter1 = "30";
+      let pricePerLiter2 = "40";
+      let pricePerLiter3 = "10";
+      if (arrayResult.isEmpty) {
+        arrayResult.append(contentsOf: [userMaxAllowance,fuelType1,pricePerLiter1,fuelType2,pricePerLiter2,fuelType3,pricePerLiter3])
+      }
+      resolve(arrayResult)
+    }
+    catch{
+      reject("ERROR", "Cannot get device info", error)
+    }
+  }
+  
+  @objc
+  func getBlanceCheck(_ userAvailableBalance: Int, userMaxAllowance: Int,resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
+      if(userAvailableBalance > userMaxAllowance){
+          resolve(true)
+
+      }else{
+          resolve(false)
+      }
+  }
+}
